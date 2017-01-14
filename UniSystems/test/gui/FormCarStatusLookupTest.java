88cd3d46cd89b74459775ee6ec366cd7009d15c5
s4dko <S4dko@hotmail.co.uk>
2017-01-14 16:58:02 +0000

    test library

---
+++ UniSystems/test/gui/FormCarStatusLookupTest.java
@@ -0,0 +1,49 @@
+/*
+ *  UniSystems is a development package for SOFT 252 at Plymouth University
+ *  It is a system designed to allow cars to be loaned out on a custom basis to 
+ *  a member of staff. This project was created and developed by Craig Banyard,
+ *  Daniel Scott and Najim Mazidi.
+ */
+package gui;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Craig
+ */
+public class FormCarStatusLookupTest {
+    
+    public FormCarStatusLookupTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testPopulateList() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
---
+++ UniSystemsDataModel/test/car/CarTest.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Craig
+ */
+public class CarTest {
+    
+    public CarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testNeedsService() {
+    }
+
+    @Test
+    public void testGetMake() {
+    }
+
+    @Test
+    public void testSetMake() {
+    }
+
+    @Test
+    public void testGetModel() {
+    }
+
+    @Test
+    public void testSetModel() {
+    }
+
+    @Test
+    public void testSetMilage() {
+    }
+
+    @Test
+    public void testGetRegNo() {
+    }
+
+    @Test
+    public void testGetStatus() {
+    }
+
+    @Test
+    public void testSetStatus() {
+    }
+
+    @Test
+    public void testGetClassification() {
+    }
+
+    @Test
+    public void testGetLocation() {
+    }
+
+    @Test
+    public void testGetParkingSpace() {
+    }
+
+    @Test
+    public void testSetParkingSpace() {
+    }
+
+    @Test
+    public void testGetMilage() {
+    }
+
+    @Test
+    public void testGetTransmission() {
+    }
+
+    @Test
+    public void testGetFuelType() {
+    }
+
+    @Test
+    public void testGetSeats() {
+    }
+
+    @Test
+    public void testGetDoors() {
+    }
+
+    @Test
+    public void testGetLoanType() {
+    }
+
+    @Test
+    public void testSetLoanType() {
+    }
+
+    @Test
+    public void testGetServiceRecord() {
+    }
+
+    @Test
+    public void testAddService() {
+    }
+
+    @Test
+    public void testAddNote() {
+    }
+
+    @Test
+    public void testAddNotes() {
+    }
+
+    @Test
+    public void testGetNotes() {
+    }
+    
+}
